09f3d24c259808acb1786b9e53ef5e2cd8497106
erik <erikvb@vbits.nl>
2026-01-06 20:17:21 +0100

    worked on tests

--- README.md
+++ README.md
@@ -2,25 +2,14 @@
 BUGS
 
 - in deterministic mode, I still see variations
-- improve the heuristics by adding tests for bad moves
 
 TODO
 
+- improve the heuristics by adding tests for bad moves
 - make it smart enough to play a descend end game
 
 
 TESTS
-
-Filter by test name substring (across unit + integration tests):
-
-cargo test perft_position3_depth
-
-other test commands:
-
-cargo test
-cargo test --test '*'
-cargo test --test perft_depth_1_2_and_3 --test move_tests --test pv_test --test blunder_tests --no-capture
-
 
 powershell script to run all tests in a directory:
 
--- tests/chess/perft_depth_4_5_and_higher.rs
+++ tests/chess/perft_depth_4_5_and_higher.rs
@@ -7,9 +7,9 @@ use chess::search::advanced_search::_dump_all_valid_moves;
 /// perft tests, see https://www.chessprogramming.org/Perft_Results
 ///
 
-//#[test]
+#[test]
 #[serial]
-fn perft_position2_depth4() {
+fn perft_position2_depth4() { //last tested at 6-jan-2026
     let fen = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
     let gs = reset_from_fen(fen).expect("valid FEN");
     assert_eq!(perft_count(&gs, 4), 4_085_603);
@@ -21,13 +21,13 @@ fn perft_position2_depth4() {
 fn perft_position2_depth5() {
     let fen = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
     let gs = reset_from_fen(fen).expect("valid FEN");
-    //assert_eq!(perft_count(&gs, 5), 193_690_690); //wiki
-    assert_eq!(perft_count(&gs, 5), 193_699_275); //TODO this version?? diff = 8585???? check with stockfish?
+    assert_eq!(perft_count(&gs, 5), 193_690_690); // stockfish: Nodes searched: 193690690
+    //assert_eq!(perft_count(&gs, 5), 193_699_275); //TODO this version?? diff = 8585???? promotions?
 }
 
 #[test]
 #[serial]
-fn perft_position3_depth4() {
+fn perft_position3_depth4() { // last tested at 6-jan-2026
     let fen = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1";
     let gs = reset_from_fen(fen).expect("valid FEN");
     assert_eq!(perft_count(&gs, 4), 43_238);
@@ -35,7 +35,7 @@ fn perft_position3_depth4() {
 
 #[test]
 #[serial]
-fn perft_position4_depth4() {
+fn perft_position4_depth4() { // last tested at 6-jan-2026
     let fen = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1";
     let gs = reset_from_fen(fen).expect("valid FEN");
     let nodes:i64 = perft_count(&gs, 4) as i64;
@@ -47,9 +47,9 @@ fn perft_position4_depth4() {
     assert_eq!(nodes, expected);
 }
 
-//#[test]
+#[test]
 #[serial]
-fn perft_position4_depth5() {
+fn perft_position4_depth5() { // last tested at 6-jan-2026
     let fen = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1";
     let gs = reset_from_fen(fen).expect("valid FEN");
     let nodes:i64 = perft_count(&gs, 5) as i64;
@@ -62,9 +62,9 @@ fn perft_position4_depth5() {
 }
 
 
-//#[test]
+#[test]
 #[serial]
-fn perft_position5_depth4() {
+fn perft_position5_depth4() { // last tested at 6-jan-2026
     let fen = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
     let gs = reset_from_fen(fen).expect("valid FEN");
     let nodes: i64 = perft_count(&gs, 4) as i64;
@@ -79,7 +79,7 @@ fn perft_position5_depth4() {
 
 #[test]
 #[serial]
-fn perft_position6_depth4() {
+fn perft_position6_depth4() { // last tested at 6-jan-2026
     let fen = "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10";
     let gs = reset_from_fen(fen).expect("valid FEN");
     let nodes:i64 = perft_count(&gs, 4) as i64;
@@ -93,11 +93,19 @@ fn perft_position6_depth4() {
 
 #[test]
 #[serial]
-fn perft_startpos_depths() {
+fn perft_startpos_depths_4_and_5() { // last tested at 6-jan-2026
     let fen = Chess::DEFAULT_CHESS_STARTING_FEN;
     let gs = reset_from_fen(fen).expect("valid startpos FEN");
 
     assert_eq!(perft_count(&gs, 4), 197_281);
     assert_eq!(perft_count(&gs, 5), 4_865_609);
+}
+
+#[test]
+#[serial]
+fn perft_startpos_depth_6() {
+    let fen = Chess::DEFAULT_CHESS_STARTING_FEN;
+    let gs = reset_from_fen(fen).expect("valid startpos FEN");
+
     assert_eq!(perft_count(&gs, 6), 119_060_324);
 }
